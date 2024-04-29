feabfa1bc246e2c7971fb98bd86d2d65d360d5ce
Mehwishnaz <160154061+Mehwishnaz1603@users.noreply.github.com>
2024-04-29 23:31:00 +0500

    ..

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ import chalk from 'chalk';
 let students = [];
 console.log(chalk.yellowBright.bold.underline("\n\t  Welcome to the Student Management System with MehwishNaz  \n\t"));
 const student_Management_System = () => {
-    console.log(chalk.yellowBright.underline("Choose an Option:"));
     inquirer.prompt([
-        { name: 'options', type: 'list', choices: ['Add Student Information', 'List Of Student Information', 'Exit'] },
+        { name: 'options', type: 'list', message: 'Choose an Option:',
+            choices: ['Add Student Information', 'List Of Student Information', 'Exit'] },
     ])
         .then((answers) => {
         switch (answers.options) {
@@ -31,9 +31,12 @@ const addStudentInformation = () => {
     inquirer.prompt([
         { name: 'name', type: 'string', message: 'Enter Student Name:' },
         { name: 'fathername', type: 'string', message: 'Enter Student Father Name:' },
+        { name: 'age', type: 'number', message: 'Enter Student Age:', },
         { name: 'id', type: 'number', message: 'Enter Student Id:' },
-        { name: 'courses', type: 'string', message: 'Enter Student Courses:' },
+        { name: 'course', type: 'string', message: 'Enter Student Course:' },
         { name: 'day', type: 'string', message: 'Enter Student Day:' },
+        { name: 'timing', type: 'input', message: 'Enter Student Timing:', },
+        { name: 'duration', type: 'input', message: 'Enter Student Course Duration:' },
         { name: 'fees', type: 'number', message: 'Enter Student Fees:' },
         { name: 'balance', type: 'number', message: 'Enter Student Balance:' }
     ])
@@ -42,8 +45,11 @@ const addStudentInformation = () => {
             name: answers.name,
             fathername: answers.fathername,
             id: answers.id,
+            age: answers.age,
             course: answers.course,
             day: answers.day,
+            timing: answers.timing,
+            duration: answers.duration,
             fees: answers.fees,
             balance: answers.balance
         };
@@ -55,8 +61,8 @@ const addStudentInformation = () => {
 const listOfStudentsInformation = () => {
     console.log('List of Students:');
     students.forEach((student) => {
-        console.log(chalk.yellowBright(`name: ${student.name},id: ${student.id}, course: ${student.course},
-    day: ${student.day}, fees: ${student.fees}, balance: ${student.balance}`));
+        console.log(chalk.yellowBright(`name: ${student.name}, id: ${student.id}, age :${student.name}, course: ${student.course},
+    day: ${student.day}, timig: $ {student.timing}, duration: ${student.duration}, fees: ${student.fees}, balance: ${student.balance}`));
     });
     student_Management_System();
 };
--- index.ts
+++ index.ts
@@ -2,14 +2,14 @@
 import inquirer from 'inquirer';
 import chalk from 'chalk';
 
-interface Student{name:string; fathername:string; id:number; course:string; day:string; fees:number; balance:number};
+interface Student{name:string; fathername:string; age:number; id:number; course:input; 
+                 day:string; timing:number; duration:input;fees:number; balance:number};
 let students: Student[] = [];
 console.log(chalk.yellowBright.bold.underline("\n\t  Welcome to the Student Management System with MehwishNaz  \n\t"));
-
 const student_Management_System = () => {
-    console.log(chalk.yellowBright.underline("Choose an Option:"))
      inquirer.prompt([
-    {name: 'options', type: 'list', choices: ['Add Student Information', 'List Of Student Information', 'Exit'] },
+    {name: 'options', type: 'list', message: 'Choose an Option:',
+    choices: ['Add Student Information', 'List Of Student Information', 'Exit'] },
 ])
    .then((answers) => { switch (answers.options) {
                     case 'Add Student Information':
@@ -29,21 +29,27 @@ const student_Management_System = () => {
 };
     const addStudentInformation = () => {
     inquirer.prompt([
-             {name: 'name',            type: 'string',      message: 'Enter Student Name:'},
-             {name: 'fathername',      type: 'string',      message: 'Enter Student Father Name:'},
-             {name: 'id',              type: 'number',      message: 'Enter Student Id:'},
-             {name: 'courses',         type: 'string',      message: 'Enter Student Courses:'},
-             {name: 'day',             type: 'string',      message: 'Enter Student Day:'},
-             {name: 'fees',            type: 'number',      message: 'Enter Student Fees:'},
-             {name: 'balance',         type: 'number',      message: 'Enter Student Balance:'}
+                     {name: 'name',   type: 'string', message: 'Enter Student Name:'},
+                     {name: 'fathername', type: 'string', message: 'Enter Student Father Name:'},
+                     {name: 'age',    type: 'number', message: 'Enter Student Age:',}, 
+                     {name: 'id',         type: 'number', message: 'Enter Student Id:'},
+                     {name: 'course', type: 'string', message: 'Enter Student Course:'},
+                     {name: 'day',      type: 'string', message: 'Enter Student Day:'},
+                     {name: 'timing', type: 'input',  message: 'Enter Student Timing:',},
+                     {name: 'duration', type: 'input',  message: 'Enter Student Course Duration:'},
+                     {name: 'fees',   type: 'number', message: 'Enter Student Fees:'},
+                     {name: 'balance',    type: 'number', message: 'Enter Student Balance:'}
    ])
     .then((answers) => {
     const newStudent: Student = {
                     name: answers.name,
                     fathername: answers.fathername,
                     id: answers.id,
+                    age: answers.age,
                     course: answers.course,
                     day: answers.day,
+                    timing: answers.timing,
+                    duration: answers.duration,
                     fees: answers.fees,
                     balance: answers.balance };
     students.push(newStudent);
@@ -54,8 +60,8 @@ const student_Management_System = () => {
     const listOfStudentsInformation = () => {
     console.log('List of Students:');
     students.forEach((student) => {
-    console.log(chalk.yellowBright(`name: ${student.name},id: ${student.id}, course: ${student.course},
-    day: ${student.day}, fees: ${student.fees}, balance: ${student.balance}`));});
+    console.log(chalk.yellowBright(`name: ${student.name}, id: ${student.id}, age :${student.name}, course: ${student.course},
+    day: ${student.day}, timig: $ {student.timing}, duration: ${student.duration}, fees: ${student.fees}, balance: ${student.balance}`));});
     student_Management_System();
 };
-    student_Management_System();
+    student_Management_System()
